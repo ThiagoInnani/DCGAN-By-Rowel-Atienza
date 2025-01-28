54d55404f69fd4c3ef2dd56a17b546339d1ed0c0
ThiagoInnani <thiagoinnani@gmail.com>
2025-01-28 09:58:19 -0300

    v2 - updated the size of the output to 1024 x 1024

--- dcgan.py
+++ dcgan.py
@@ -4,19 +4,20 @@ Implementation based on the code by Rowel Atienza https://towardsdatascience.com
 '''
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+from tqdm import tqdm  # Biblioteca para mostrar o progresso
 from tensorflow import keras
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Activation, Flatten, Reshape, Conv2D, Conv2DTranspose, UpSampling2D, LeakyReLU, Dropout
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.optimizers import Adam, RMSprop
-from tensorflow.keras.utils import array_to_img
+from tensorflow.keras.models import Sequential, load_model # type: ignore
+from tensorflow.keras.layers import Dense, Activation, Flatten, Reshape, Conv2D, Conv2DTranspose, UpSampling2D, LeakyReLU, Dropout   # type: ignore
+from tensorflow.keras.layers import BatchNormalization # type: ignore
+#from tensorflow_addons.layers import SpectralNormalization  # Install if missing
+from tensorflow.keras.optimizers import Adam, RMSprop # type: ignore
+from tensorflow.keras.utils import array_to_img # type: ignore
 #from keras._tf_keras.keras.preprocessing.image import array_to_img (versões antigas só)
 #from tensorflow.keras.utils import plot_model
-import matplotlib.pyplot as plt
-from tqdm import tqdm  # Biblioteca para mostrar o progresso
-import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Remove logs INFO
-import tensorflow as tf
 
 
 
@@ -33,8 +34,7 @@ class DCGAN(object):
         self.AM = None  # adversarial model
         self.DM = None  # discriminator model
 
-    def discriminator_block(self, depth, input_shape=None, alpha=0.2, dropout = 0.4):
-
+    def discriminator_block(self, depth, input_shape=None, alpha=0.2, dropout=0.4):
         block = Sequential()
         if input_shape:
             block.add(Conv2D(depth, 5, strides=2, input_shape=input_shape, padding='same'))
@@ -42,7 +42,6 @@ class DCGAN(object):
             block.add(Conv2D(depth, 5, strides=2, padding='same'))
         block.add(LeakyReLU(alpha=alpha))
         block.add(Dropout(dropout))
-
         return block
 
     # Define the discriminator network
@@ -50,34 +49,29 @@ class DCGAN(object):
         if self.D:
             return self.D
         self.D = Sequential()
-        depth = 64
+        depth = 32  # Reduced for memory
         input_shape = (self.img_rows, self.img_cols, self.channels)
 
-        self.D.add( self.discriminator_block(depth, input_shape=input_shape) )
-        self.D.add( self.discriminator_block(depth*2) )
-        self.D.add( self.discriminator_block(depth*4) )
-        self.D.add( self.discriminator_block(depth*8) )
+        # Downsample from 1024x1024 to 32x32
+        self.D.add(self.discriminator_block(depth, input_shape=input_shape))    # 1024 → 512
+        self.D.add(self.discriminator_block(depth * 2))                         # 512 → 256
+        self.D.add(self.discriminator_block(depth * 4))                         # 256 → 128
+        self.D.add(self.discriminator_block(depth * 8))                         # 128 → 64
+        self.D.add(self.discriminator_block(depth * 16))                        # 64 → 32
 
-        # Out: 1-dim probability
+        # Output layer
         self.D.add(Flatten())
         self.D.add(Dense(1))
         self.D.add(Activation('sigmoid'))
-
-        print("Discriminator summary")
-        self.D.summary()
-        #plot_model(self.D, "model.png") # Plot model
-
-        return self.D
+        return self.D  # Critical: Return the discriminator!
 
     def generator_block(self, depth, upsampling=True):
-
         block = Sequential()
         if upsampling:
             block.add(UpSampling2D())
         block.add(Conv2DTranspose(int(depth), 5, padding='same'))
         block.add(BatchNormalization(momentum=0.9))
         block.add(Activation('relu'))
-
         return block
 
     # Define the generator network
@@ -86,39 +80,40 @@ class DCGAN(object):
             return self.G
         self.G = Sequential()
         dropout = 0.4
-        depth = 64*4
-        dim = int(self.img_rows/4) # change for non-square images
+        depth = 64 * 4  # Initial depth
+        dim = int(self.img_rows / 16)  # 1024/16 = 64
         input_dim = 100
 
-        # In: 100
-        # Out: dim x dim x depth
-        self.G.add(Dense(dim*dim*depth, input_dim=input_dim))
+        # Input layer
+        self.G.add(Dense(dim * dim * depth, input_dim=input_dim))
         self.G.add(BatchNormalization(momentum=0.9))
         self.G.add(Activation('relu'))
         self.G.add(Reshape((dim, dim, depth)))
         self.G.add(Dropout(dropout))
 
-        # In: dim x dim x depth
-        # Each generator block halves the depth, and doubles the dimensions if there is upsampling
-        # Final image will be dim*4=self.img_rows
-        self.G.add( self.generator_block(depth/2) )
-        self.G.add( self.generator_block(depth/4) )
-        self.G.add( self.generator_block(depth/8, upsampling=False) )
+        # Upsampling blocks (4 blocks to reach 1024x1024)
+        self.G.add(self.generator_block(depth // 2))    # 64x64 → 128x128
+        self.G.add(self.generator_block(depth // 4))    # 128x128 → 256x256
+        self.G.add(self.generator_block(depth // 8))    # 256x256 → 512x512
+        self.G.add(self.generator_block(depth // 16))   # 512x512 → 1024x1024
 
-        # Out: self.img_rows x self.img_rows x self.channels image
+        # Final output layer
         self.G.add(Conv2DTranspose(self.channels, 5, padding='same'))
         self.G.add(Activation('sigmoid'))
 
         print("Generator summary")
         self.G.summary()
-
-        return self.G
+        return self.G  # Ensure this line is present!
 
     # Define the discriminator model
     def discriminator_model(self):
         if self.DM:
             return self.DM
-        optimizer = RMSprop(learning_rate=0.0002, decay=6e-8)
+        
+        #Otimizador: escolha um dos dois
+        #optimizer = RMSprop(learning_rate=0.0002, decay=6e-8)
+        optimizer = Adam(learning_rate=0.0002, beta_1=0.5)
+
         self.DM = Sequential()
         self.DM.add(self.discriminator())
         self.DM.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
@@ -128,7 +123,11 @@ class DCGAN(object):
     def adversarial_model(self):
         if self.AM:
             return self.AM
-        optimizer = RMSprop(learning_rate=0.0001, decay=3e-8)
+        
+        #Otimizador: escolha um dos dois
+        #optimizer = RMSprop(learning_rate=0.0001, decay=3e-8)
+        optimizer = Adam(learning_rate=0.0001, beta_1=0.5)
+
         self.AM = Sequential()
         self.AM.add(self.generator())
         disc = self.discriminator()
@@ -146,6 +145,8 @@ class Image_DCGAN(object):
         self.img_rows = images.shape[1]
         self.img_cols = images.shape[2]
         self.channels = images.shape[3] # 3 if RGB, 1 if grayscale
+
+        print(f'\n\n\n\n\nIMGROWS: {self.img_rows}, IMGCOLS: {self.img_cols}, CHANNELS: {self.channels}\n\n\n\n\n\n\n')
 
         self.DCGAN = DCGAN(rows=self.img_rows, cols=self.img_cols, channels=self.channels)
 
@@ -179,11 +180,14 @@ class Image_DCGAN(object):
         # Adicione tqdm para visualização do progresso
         for i in tqdm(range(train_steps), desc="Training Progress", unit="epoch"):
             images_train = self.x_train[np.random.randint(0, self.x_train.shape[0], size=batch_size), :, :, :]
+            
             noise = np.random.uniform(0., 1.0, size=[batch_size, 100])
             images_fake = self.generator.predict(noise)
+        
 
             # Concatenate real and fake images for computing training the discriminator
             x = np.concatenate((images_train, images_fake))
+            #x = np.concatenate((images_train, images_fake))
             y = np.ones([2 * batch_size, 1])
             y[batch_size:, :] = 0  # Fake images labeled as 0
             d_loss = self.discriminator.train_on_batch(x, y)
@@ -214,7 +218,7 @@ class Image_DCGAN(object):
 
         return d_losses, a_losses, d_acc, a_acc
 
-    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
+    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0, individual=False):
         filename = 'outputs/image_sample'#.png'
 
         if fake:
@@ -224,26 +228,51 @@ class Image_DCGAN(object):
             else:
                 filename = "outputs/image_step_%d.png" % step
             images = self.generator.predict(noise)
+            #images = tf.image.resize(images_brute, (1024, 1024)).numpy()
+            
         else:
             filename+="_true.png"
             i = np.random.randint(0, self.x_train.shape[0], samples)
             images = self.x_train[i, :, :, :]
 
-        plt.figure(figsize=(10,10))
-        for i in range(images.shape[0]):
-            plt.subplot(4, 4, i+1)
-            image = images[i, :, :, :]
-            #image = np.reshape(image, [self.img_rows, self.img_cols])
-            img = array_to_img(image)
-            plt.imshow(img)
-            plt.axis('off')
-        plt.tight_layout()
+        if individual:
+            for idx, img in enumerate(images):
+                # Verificar se a imagem precisa de ajustes
+                if isinstance(img, np.ndarray):
+                    if img.dtype != np.uint8:
+                        img = (img * 255).clip(0, 255).astype(np.uint8)  # Normalizar
+                    if img.ndim == 4:
+                        img = img[0]  # Pegar a primeira imagem
+                    if img.ndim == 3 and img.shape[-1] == 1:  # Remover canal único
+                        img = np.squeeze(img, axis=-1)
+                elif not isinstance(img, Image.Image):
+                    raise ValueError("Imagem inválida. Deve ser um array NumPy ou objeto PIL.")
+
+                # Converter para PIL.Image
+                pil_img = Image.fromarray(img)
+
+                # Redimensionar
+                resized_img = pil_img.resize((1024, 1024), Image.Resampling.LANCZOS)
+
+                # Salvar a imagem
+                output_path = os.path.join('outputs/individual_images', f"image_{idx+1}.png")
+                resized_img.save(output_path, "PNG", compress_level=0)
+                print(f"Imagem {idx+1} salva em {output_path}")
+        else:
+            plt.figure(figsize=(10,10))
+            for i in range(images.shape[0]):
+                plt.subplot(4, 4, i+1)
+                image = images[i, :, :, :]
+                #image = np.reshape(image, [self.img_rows, self.img_cols])
+                img = array_to_img(image)
+                plt.imshow(img)
+                plt.axis('off')
+            plt.tight_layout()
         if save2file:
             plt.savefig(filename)
             plt.close('all')
         else:
             plt.show()
-
 
     def plot_loss_acc(self, d_losses, a_losses, d_acc, a_acc):
 
--- main_gan.py
+++ main_gan.py
@@ -1,20 +1,13 @@
-"""
-Image generator using a DCGAN
-Author: Pablo Villanueva Domingo
-Last update: 28/5/2020
-"""
-
-import time, datetime, glob, os
-#from keras._tf_keras.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.models import load_model
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Remove logs INFO
+import time, datetime, glob, os
 import tensorflow as tf
-
-
+from tensorflow.keras.preprocessing.image import load_img, img_to_array # type: ignore
+from tensorflow.keras.models import load_model # type: ignore
 from dcgan import *
 
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Remove logs INFO
 time_ini = time.time()
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
@@ -22,7 +15,6 @@ tf.debugging.set_log_device_placement(True)
 
 # Load the images, normalize them to [0,1] and convert them to np.array
 def load_images(datapath, newsize):
-
     images = glob.glob(datapath)
     imarrays = []
     for image in images:
@@ -36,34 +28,27 @@ def load_images(datapath, newsize):
 
 
 #--- PARAMETERS ---#
-
-# Number of epochs
-n_epochs = 5000
-# Batch size
-batch_size = 64
-# 1 for loading a previously trained model
-load_prev_model = 1
-# 1 for trainig the network
-train_model = 1
-# Resize the images to save some memory and time (It has to be multiple of 2, otherwise it fails sometimes...)
-newsize = 128
+n_epochs = 15000 # Number of epochs
+batch_size = 4 # Batch size
+load_prev_model = 0 # 1 for loading a previously trained model
+train_model = 1 # 1 for trainig the network
+newsize = 1024 # Resize the images to save some memory and time 
+#(It has to be multiple of 2, otherwise it fails sometimes...)
 
 #--- MAIN ---#
-
 # Ensure that you are using GPU if available. Otherwise, it will take some time to run
 with tf.device('/GPU:0'):
-
     # Create some directories to store the outputs and models, and unzip the dataset
     if not os.path.isdir("outputs"):
         os.mkdir("outputs")
     if not os.path.isdir("models"):
         os.mkdir("models")
-    #if not os.path.isdir("flower_images"):
-    #    os.system("unzip flower_images.zip")
+    if not os.path.isdir("outputs/individual_images"):
+        os.mkdir("outputs/individual_images")
 
     # Load the data
     datapath = "images/DJI*"
-    imarrays = load_images(datapath, newsize)
+    imarrays = load_images(datapath, newsize=1024)
 
     # Initialize the GAN
     model_dcgan = Image_DCGAN(imarrays, load_prev_model=load_prev_model)
@@ -77,7 +62,9 @@ with tf.device('/GPU:0'):
         model_dcgan.plot_loss_acc(d_losses, a_losses, d_acc, a_acc)
 
     # Plot some true and fake sample images
+    model_dcgan.plot_images(fake=True, save2file=True, individual=True)
     model_dcgan.plot_images(fake=True, save2file=True)
     model_dcgan.plot_images(fake=False, save2file=True)
+
 
 print("Finished. Time elapsed:",datetime.timedelta(seconds=time.time()-time_ini))
